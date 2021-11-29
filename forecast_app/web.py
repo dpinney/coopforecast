4541e6770d74f937b25f2f22f2b3825f34fa6687
kmcelwee <kevinrmcelwee@gmail.com>
2021-11-29 12:28:01 -0500

    Stub main views

--- forecast_app/web.py
+++ forecast_app/web.py
@@ -4,9 +4,24 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return render_template("login.html")
+def login():
+    return render_template("login.html", name="login")
     # return "Hello World!"
+
+
+@app.route("/load-data")
+def load_data():
+    return render_template("load-data.html", name="load-data")
+
+
+@app.route("/weather-data")
+def weather_data():
+    return render_template("weather-data.html", name="weather-data")
+
+
+@app.route("/forecast")
+def forecast():
+    return render_template("forecast.html", name="forecast")
 
 
 if __name__ == "__main__":
