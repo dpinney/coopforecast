6bc27baa533accec8450ea143f469fa37e00f312
kmcelwee <kevinrmcelwee@gmail.com>
2021-12-13 15:17:58 -0500

    Allow templates to handle logic for no data provided, closes #44

--- forecast_app/tests/test_views.py
+++ forecast_app/tests/test_views.py
@@ -8,7 +8,7 @@ from forecast_app.views import ForecastView, LoadDataView, WeatherDataView
 
 
 class TestLoadDataView:
-    def test_get_table(self):
+    def test_get_table(self, db):
         chart_array = LoadDataView().get_chart()
         assert type(chart_array) == list
         assert all([len(datapoint) == 2 for datapoint in chart_array])
