28931057479523811133132ef8490573d91d57aa
kmcelwee <kevinrmcelwee@gmail.com>
2021-12-29 12:57:06 -0500

    Collapse JS

---
+++ forecast_app/static/js/main.js
@@ -0,0 +1,28 @@
+function create_timeseries_chart(id = null, data = null, title = null, series_name = null) {
+  // Create the chart
+  Highcharts.stockChart(id, {
+    rangeSelector: {
+      selected: 1
+    },
+
+    title: {
+      text: title
+    },
+
+    series: [{
+      name: series_name,
+      data: data,
+      tooltip: {
+        valueDecimals: 2
+      }
+    }]
+  });
+};
+
+function toggle_table_visibility() {
+  $('#table-warning').toggleClass('d-none');
+  $('#large-table').toggleClass('d-none');
+
+  text = $('#table-button').text()
+  $('#table-button').text(text == "Hide Table" ? "Show Table" : "Hide Table")
+}
