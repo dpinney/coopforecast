6e1913ab2254b4ee3518b17e375d5608e0130217
kmcelwee <kevinrmcelwee@gmail.com>
2022-01-28 13:42:36 -0500

    Build NWS API wrapper

--- forecast_app/tests/test_weather.py
+++ forecast_app/tests/test_weather.py
@@ -6,7 +6,14 @@ import filecmp
 import pandas as pd
 
 from forecast_app import create_app
-from forecast_app.weather import AsosRequest
+from forecast_app.weather import AsosRequest, NwsForecastRequest
+
+# https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response
+class MockResponse:
+    def __init__(self, mock_path, status_code):
+        self.status_code = status_code
+        with open(mock_path, "r") as f:
+            self.text = f.read()
 
 
 class TestAsosRequest:
@@ -30,13 +37,6 @@ class TestAsosRequest:
         assert df["station"].iloc[0] == app.config["ASOS_STATION"]
 
     def mocked_asos_response(*args, **kwargs):
-        # https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response
-        class MockResponse:
-            def __init__(self, mock_path, status_code):
-                self.status_code = status_code
-                with open(mock_path, "r") as f:
-                    self.text = f.read()
-
         mock_path = pytest.FIXTURE_DIR / "asos-response.csv"
         return MockResponse(mock_path, 200)
 
@@ -79,3 +79,36 @@ class TestAsosRequest:
         asos_request.write_response(tmp_output_path)
         assert os.path.exists(tmp_output_path)
         assert filecmp.cmp(mock_path, tmp_output_path)
+
+
+class TestNwsForecastRequest:
+    def mocked_nws_response(*args, **kwargs):
+        mock_path = pytest.FIXTURE_DIR / "nws-response.json"
+        return MockResponse(mock_path, 200)
+
+    @patch("requests.get", side_effect=mocked_nws_response)
+    def test_send_request(self, mock_get):
+        nws_request = NwsForecastRequest().send_request()
+        assert nws_request.text.startswith("{")
+
+    @patch("requests.get", side_effect=mocked_nws_response)
+    def test_create_df(self, mock_get):
+        nws_request = NwsForecastRequest()
+        nws_request.send_request()
+        df = nws_request.create_df()
+        assert df.shape[0] == 156
+        assert df["tempc"].iloc[0] == 2.78
+
+    @patch("requests.get", side_effect=mocked_nws_response)
+    def test_write_response(self, mock_get, app):
+        nws_request = NwsForecastRequest()
+        nws_request.send_request()
+        tmp_output_path = app.config["OUTPUT_DIR"] + "/nws-response.json"
+        mock_path = pytest.FIXTURE_DIR / "nws-response.json"
+        nws_request.write_response(tmp_output_path)
+        assert os.path.exists(tmp_output_path)
+        assert filecmp.cmp(mock_path, tmp_output_path)
+
+    def test_fahrenheit_to_celcius(self):
+        assert NwsForecastRequest.fahrenheit_to_celcius(32) == 0
+        assert NwsForecastRequest.fahrenheit_to_celcius(14) == -10
--- forecast_app/weather.py
+++ forecast_app/weather.py
@@ -5,6 +5,7 @@ A collection of utilities to help collect and parse historical and forecast weat
 import requests
 from io import StringIO
 import pandas as pd
+import json
 
 
 class AsosRequest:
@@ -77,4 +78,56 @@ class AsosRequest:
         return pd.DataFrame(series)
 
 
-# Given a start datetime and a zipcode, collect the next 24 hours of forecasted temperature data.
+class NwsForecastRequest:
+    # TODO: Can this be an abstract class?
+    """Pulls hourly data from the National Weather Service
+    Docs: https://weather-gov.github.io/api/
+    Example request: https://api.weather.gov/gridpoints/LWX/96,70/forecast/hourly
+    * Timezone data is encoded in the response as UTC with offset. We strip it.
+    * The temperature is in Fahrenheit, we convert to Celsius.
+    * 6.5 days of forecast data is provided.
+    """
+
+    base_url = "https://api.weather.gov/gridpoints/"
+
+    def __init__(self, nws_code=""):
+        self.nws_code = nws_code
+
+    @classmethod
+    def fahrenheit_to_celcius(cls, fahrenheit):
+        # TODO: We should be using fahrenheit instead of celcius, but it's baked into
+        #  a lot of the structure. May not be worth switching back for a while.
+        return round((fahrenheit - 32) * 5 / 9, 2)
+
+    def send_request(self):
+        self.request = requests.get(self.base_url + self.nws_code + "/forecast/hourly")
+        if self.request.status_code == 404:
+            raise Exception(f"Dataset URL does not exist. {self.request.url}")
+        return self.request
+
+    def write_response(self, filepath):
+        self.filepath = filepath
+        if not hasattr(self, "request"):
+            raise Exception("No request has been sent yet.")
+        if not self.request.text:
+            raise Exception(f"No data found. {self.request.url}")
+        with open(filepath, "w") as f:
+            f.write(self.request.text)
+
+    def create_df(self):
+        if not hasattr(self, "request"):
+            raise Exception("No request has been sent yet.")
+        json_response = json.loads(self.request.text)
+        dict_list = []
+        for item in json_response["properties"]["periods"]:
+            item = {
+                # Removing tz info from timestamp. This makes the strong assumption
+                #  that NWS will always correctly provide data in the timezone of
+                #  the station we're pulling from.
+                "timestamp": pd.to_datetime(item["startTime"]).replace(tzinfo=None),
+                "tempc": self.fahrenheit_to_celcius(item["temperature"]),
+            }
+            dict_list.append(item)
+        df = pd.DataFrame(dict_list)
+        df.set_index("timestamp", inplace=True)
+        return df
