aea4cdeb9099b2025cdccb7e2297b0babaf875b8
kmcelwee <kevinrmcelwee@gmail.com>
2022-02-11 07:22:15 -0500

    Add full path to cronjob, closes #116

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ export DOMAIN=$(python3 -c "from forecast_app.config import DOMAIN; print(DOMAIN
 if [ $(crontab -l | grep -v "^#" | wc -l) -eq 0 ]; then
     export ADMIN_USER=$(python3 -c "from forecast_app.secret_config import ADMIN_USER; print(ADMIN_USER)")
     export ADMIN_PASSWORD=$(python3 -c "from forecast_app.secret_config import ADMIN_PASSWORD; print(ADMIN_PASSWORD)")
-    export WEATHER_SYNC_CRON="0 * * * * /usr/bin/python3 cli.py sync-weather-data --username $ADMIN_USER --password='$ADMIN_PASSWORD' --url https://coopforecast.com"
+    export WEATHER_SYNC_CRON="0 * * * * /usr/bin/python3 $REPO/cli.py sync-weather-data --username $ADMIN_USER --password='$ADMIN_PASSWORD' --url https://coopforecast.com"
     (crontab -l ; echo "$WEATHER_SYNC_CRON")| crontab -
 fi
 
