bbb414afaa49c6ebe81d3e2ebba0e909b54cd09d
Ubuntu <ubuntu@ip-172-30-0-98.ec2.internal>
2022-06-21 14:32:39 -0500

    acme challenge

--- redirect.py
+++ redirect.py
@@ -3,7 +3,7 @@ Small redirect app to redirect HTTP traffic to HTTPS.
 See CLI for usage and configuration
 """
 
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, send_from_directory
 
 reApp = Flask("HTTPS_REDIRECT")
 
@@ -15,6 +15,12 @@ def index():
 
 @reApp.before_request
 def before_request():
-    if request.url.startswith("http://"):
+    if '/.well-known/acme-challenge' in request.url:
+        try:
+            filename = request.url.split('/')[-1]
+        except:
+            filename = 'none'
+        return send_from_directory('/opt/coopforecast/forecast_app/.well-known/acme-challenge', filename)
+    elif request.url.startswith("http://"):
         url = request.url.replace("http://", "https://", 1)
         return redirect(url, code=301)
